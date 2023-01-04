80b52d47368991323ae4925647db0dc3bf9b5fbe
YunL97 <leeys9932@naver.com>
2023-01-04 11:37:24 +0900

    swift 네이티브 코드변경해보기

--- ios/Runner/ViewController.swift
+++ ios/Runner/ViewController.swift
@@ -10,22 +10,44 @@ import UIKit
 import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
 
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - Selectors
+    @objc func handleLogin() {
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        print("aaaㅁㅁㅁㅁaa")
     }
-    */
+    
+    //MARK: - Helpers
+    func configureUI() {
+        view.addSubview(loginButton)
+        loginButton.anchor( left: view.leftAnchor,bottom: view.bottomAnchor,right: view.rightAnchor ,paddingLeft: 40, paddingRight: 40)
+        
+    }
 
+    
 }
